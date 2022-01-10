4f45bcfe134aa57a7f5762e6f606885f83ee52ca
cacticato <elisahonoratolira@uc.cl>
2022-01-10 15:50:08 -0300

    preload

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
  // SCROLL
 var secundario = document.getElementById("secundario").offsetHeight;
 window.addEventListener("scroll", (event) => {
@@ -445,3 +446,39 @@ window.addEventListener("scroll", (event) => {
 	
 	
 })(jQuery);
+=======
+/*!
+* Start Bootstrap - New Age v6.0.5 (https://startbootstrap.com/theme/new-age)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-new-age/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+>>>>>>> 0ccbc51 (x)
